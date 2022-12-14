8a740a34b2c13df857455d264f6360343d4cbe94
Dakshatha0 <kasturidakshatha@gmail.com>
2022-12-14 14:27:16 +0530

    initial commit

--- 3.Repetitions/simple loops/CaesarCipher.cpp
+++ 3.Repetitions/simple loops/CaesarCipher.cpp
@@ -6,8 +6,31 @@ int main(){
     cout << "enter str: ";
     cin >> str;
     for(int i = 0; i < str.length(); i++){
-        encryptedString = str[i] + 3;
+        if(str[i] == 'X'){
+            cout << 'A';
+        }
+        else if(str[i] == 'Y'){
+            cout << 'B';
+        }
+        else if(str[i] == 'Z'){
+            cout << 'C';
+        }
+        if(str[i] == 'x'){
+            cout << 'a';
+        }
+        else if(str[i] == 'y'){
+            cout << 'b';
+        }
+        else if(str[i] == 'z'){
+            cout << 'c';
+        }
+        if(isalpha(str[i])){
+            str[i] = str[i] + 3;
+            cout << str[i];
+        }
+        else{
+            cout << " ";
+        }
     }
-    cout << encryptedString << endl;
     return 0;
 }
