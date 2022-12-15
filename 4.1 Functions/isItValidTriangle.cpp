52ff59848c16189295ebc4b20ee612a3cc4c668f
Dakshatha0 <kasturidakshatha@gmail.com>
2022-12-15 20:23:22 +0530

    initial commit

--- 4.1 Functions/isItValidTriangle.cpp
+++ 4.1 Functions/isItValidTriangle.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool isValidTriangle(int s1,int s2,int s3){
-    if(s1 + s2 >= s3 || s1 + s3 >= s2 || s2 + s3 >= s1 || s1 <= 0 || s2 <= 0 || s3 <= 0){
+    if(s1 >= s2 + s3 || s3 >= s1 + s2 || s2 >= s1 + s3 || s1 <= 0 || s2 <= 0 || s3 <= 0){
         return false;
     }
     return true;
