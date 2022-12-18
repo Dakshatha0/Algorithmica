ecdd520f22c09191e795ac686a716ed12343ebf9
Dakshatha0 <kasturidakshatha@gmail.com>
2022-12-18 13:21:20 +0530

    initial commit

--- 4.2 Functions/collinearPoints.cpp
+++ 4.2 Functions/collinearPoints.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool isCollinear(int x1,int x2,int x3, int y1, int y2,int y3){
+    int m1,m2;
+    m1 = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+    if(m1 == 0){
+        return true;
+    }
+    return false;
+}
 int main(){
-    
+    int x1,x2,x3,y1,y2,y3;
+    cout << "enter: ";
+    cin >> x1 >> x2 >> x3 >> y1 >> y2 >> y3;
+    cout << isCollinear(x1,x2,x3,y1,y2,y3);
+
     return 0;
 }
--- 4.2 Functions/medianOfThreeValues.cpp
+++ 4.2 Functions/medianOfThreeValues.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n1,n2,n3;
+    cout << "enter: ";
+    cin >> n1 >> n2 >> n3;
+    if(n1 > n2 && n1 > n3){
+        n3 == n1;
+    }
+    else if(n2 > n1 && n2 > n3){
+        n3 == n2;
+    }
+    else if(n3 < n1 && n3 < n2){
+        n1 == n3;
+    }
+    else if(n2 < n1 && n2 < n3){
+        n1 == n2;
+    }
+    else if(n1 < n3 && n1 > n2 || n1 < n2 && n1 > n3){
+        n2 == n1;
+    }
+    else if(n3 < n2 && n3 > n1 || n3 < n1 && n3 > n2){
+        n2 == n3;
+    }
+    cout << "n1: " << n1 << " n2: " << n2 << " n3: " << n3;
     return 0;
 }
