d1291a4736a50eb1a8c78268aa846482b243d360
Dakshatha0 <kasturidakshatha@gmail.com>
2022-12-21 14:55:14 +0530

    initial commit

--- 1.Expressions/bottleDeposits.cpp
+++ 1.Expressions/bottleDeposits.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int numOfContainers; cout << "Enter the number of containers: ";
-    cin >> numOfContainers;
-    
+    int a,b; cout << "Enter the number of containers: ";
+    cin >> a >> b;
+    cout << "$" << a*0.10 + b*0.10; 
     return 0;
 }
