de8efc458120db7e832d1c3d8739b1e87d826545
Dakshatha0 <kasturidakshatha@gmail.com>
2022-12-28 15:50:40 +0530

    initial commit

--- 5.strings/mysteryLength.cpp
+++ 5.strings/mysteryLength.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 string removeMysteryLength(string s){
-    
+    int l = s.length();
+    for(int i = l - 1; i >= 0; i--){
+        l--;
+        if(l == stoi(s.substr(i))){
+            return s.substr(0, l);
+        }
+    }
+    return "";
 }
 int main(){
     string str;
