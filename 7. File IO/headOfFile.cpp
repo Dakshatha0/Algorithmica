b1d872aba890368b4ebab9ee9f9d2c19da9e7cfb
Dakshatha0 <kasturidakshatha@gmail.com>
2022-12-30 15:47:41 +0530

    initial commit

---
+++ 7. File IO/headOfFile.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+    string filename;
+    fstream fileC;
+    int count = 0;
+    fileC.open("headOfFile.txt");
+    if(!fileC.is_open())
+        return -1;
+    int numOfLines = 0;
+    while(getline(fileC, filename) && numOfLines < 10){
+        cout << filename << endl;     
+        numOfLines++;
+    }
+    
+    return 0;
+}
