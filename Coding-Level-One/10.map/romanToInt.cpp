99343b15532ef6486c17c7c2acebe5d55a9330ee
Dakshatha0 <kasturidakshatha@gmail.com>
2023-01-12 22:41:12 +0530

    commit

--- Coding-Level-One/10.map/romanToInt.cpp
+++ Coding-Level-One/10.map/romanToInt.cpp
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 int romanToInt(string s){
-    int count = 0;
-    unordered_map<char, int> umap;
-            umap['I'] = 1;
-            umap['V'] = 5;
-            umap['X'] = 10;
-            umap['L'] = 50;
-            umap['C'] = 100;
-            umap['D'] = 500;
-            umap['M'] = 1000;
-    for(auto i:s){
-        count += umap[i];
+    if(s.empty()) return 0;
+    unordered_map<char, int> umap= { { 'I' , 1 },
+                                   { 'V' , 5 },
+                                   { 'X' , 10 },
+                                   { 'L' , 50 },
+                                   { 'C' , 100 },
+                                   { 'D' , 500 },
+                                   { 'M' , 1000 } };
+    
+    int count = umap[s[s.length()-1]];
+    for(int i = s.length()-2; i >= 0; i--){
+        if(umap[s[i]] < umap[s[i+1]]){
+            count -= umap[s[i]];
+        }
+        else count += umap[s[i]];
     }
     return count;
 }
 int main(){
     string s;
     cout << "enter str: ";
-    getline(cin,s);
+    cin >> s;
     cout << romanToInt(s);
     return 0;
 }
